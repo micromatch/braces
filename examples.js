b50928aa6e19fc9f4ba18dcb8e52e3aee8b7b51e
jonschlinkert <dev@sellside.com>
2014-11-14 22:25:04 -0500

    adds tests for multiple sets

--- examples.js
+++ examples.js
@@ -14,12 +14,28 @@ var braces = require('./');
  * @return {Array}
  */
 
-var expand = module.exports = function expand(arr) {
+function expand1(arr) {
   arr = Array.isArray(arr) ? arr : [arr];
 
   return arr.reduce(function (acc, str) {
     return acc.concat(braces(str));
   }, [])
-};
+}
 
-console.log(expand(['{foo,bar}', '{baz,quux}', '{a,{a-{b,c}}}']));
+console.log(expand1(['{foo,bar}', '{baz,quux}', '{a,{a-{b,c}}}']));
+
+
+// faster but less readable
+function expand2(val) {
+  val = Array.isArray(val) ? val : [val];
+  var len = val.length;
+  var arr = [];
+  var i = 0;
+
+  while (i < len) {
+    arr = arr.concat(braces(val[i++]));
+  }
+  return arr;
+}
+
+console.log(expand2(['{foo,bar}', '{baz,quux}', '{a,{a-{b,c}}}']));
--- test.js
+++ test.js
@@ -36,6 +36,11 @@ describe('braces', function () {
       expand('a/b/c/{x,y}').should.eql(['a/b/c/x', 'a/b/c/y']);
     });
 
+    it('should expand multiple sets', function () {
+      expand('a{b,c{d,e}f}g').should.eql(['abg', 'acdfg', 'acefg']);
+      expand('a{b,c}d{e,f}g').should.eql(['abdeg', 'acdeg', 'abdfg', 'acdfg']);
+    });
+
     it('should expand nested sets', function () {
       expand('a{b,c{d,e},h}x/z').should.eql(['abx/z', 'acdx/z', 'ahx/z', 'acex/z']);
       expand('a{b,c{d,e},h}x{y,z}').should.eql([ 'abxy', 'acdxy', 'ahxy', 'acexy', 'abxz', 'acdxz', 'ahxz', 'acexz' ]);
@@ -51,7 +56,7 @@ describe('braces', function () {
     it('should throw an error when imbalanced braces are found.', function () {
       (function () {
         expand('a/{b,c}{d{e,f}g');
-      }).should.throw('imbalanced brace in: a/{b,c}{deg');
+      }).should.throw('[brace expansion] imbalanced brace in: a/{b,c}{deg');
     });
   });
 
@@ -59,6 +64,9 @@ describe('braces', function () {
     it('should expand numerical ranges', function () {
       expand('{1..3}').should.eql(['1', '2', '3']);
       expand('{5..8}').should.eql(['5', '6', '7', '8']);
+      expand('a{0..3}d').should.eql(['a0d', 'a1d', 'a2d', 'a3d']);
+      expand('0{a..d}0').should.eql(['0a0', '0b0', '0c0', '0d0']);
+      expand('0{a..d}0').should.not.eql(['0a0', '0b0', '0c0']);
     });
 
     it('should honor padding', function () {
@@ -78,14 +86,13 @@ describe('braces', function () {
       });
       range.should.eql(['a0', 'b1', 'c2', 'd3', 'e4']);
     });
-
-    it('should expand a complex combination of ranges and sets.', function () {
-      expand('a/{x,y}/{1..5}c{d,e}f.{md,txt}').should.eql([ 'a/x/1cdf.md', 'a/y/1cdf.md', 'a/x/2cdf.md', 'a/y/2cdf.md', 'a/x/3cdf.md', 'a/y/3cdf.md', 'a/x/4cdf.md', 'a/y/4cdf.md', 'a/x/5cdf.md', 'a/y/5cdf.md', 'a/x/1cef.md', 'a/y/1cef.md', 'a/x/2cef.md', 'a/y/2cef.md', 'a/x/3cef.md', 'a/y/3cef.md', 'a/x/4cef.md', 'a/y/4cef.md', 'a/x/5cef.md', 'a/y/5cef.md', 'a/x/1cdf.txt', 'a/y/1cdf.txt', 'a/x/2cdf.txt', 'a/y/2cdf.txt', 'a/x/3cdf.txt', 'a/y/3cdf.txt', 'a/x/4cdf.txt', 'a/y/4cdf.txt', 'a/x/5cdf.txt', 'a/y/5cdf.txt', 'a/x/1cef.txt', 'a/y/1cef.txt', 'a/x/2cef.txt', 'a/y/2cef.txt', 'a/x/3cef.txt', 'a/y/3cef.txt', 'a/x/4cef.txt', 'a/y/4cef.txt', 'a/x/5cef.txt', 'a/y/5cef.txt' ]);
-    });
   });
 
-  describe('should expand a combination of nested sets and ranges.', function () {
-    it('should expand sets', function () {
+  describe('complex', function () {
+    it('should expand a complex combination of ranges and sets:', function () {
+      expand('a/{x,y}/{1..5}c{d,e}f.{md,txt}').should.eql([ 'a/x/1cdf.md', 'a/y/1cdf.md', 'a/x/2cdf.md', 'a/y/2cdf.md', 'a/x/3cdf.md', 'a/y/3cdf.md', 'a/x/4cdf.md', 'a/y/4cdf.md', 'a/x/5cdf.md', 'a/y/5cdf.md', 'a/x/1cef.md', 'a/y/1cef.md', 'a/x/2cef.md', 'a/y/2cef.md', 'a/x/3cef.md', 'a/y/3cef.md', 'a/x/4cef.md', 'a/y/4cef.md', 'a/x/5cef.md', 'a/y/5cef.md', 'a/x/1cdf.txt', 'a/y/1cdf.txt', 'a/x/2cdf.txt', 'a/y/2cdf.txt', 'a/x/3cdf.txt', 'a/y/3cdf.txt', 'a/x/4cdf.txt', 'a/y/4cdf.txt', 'a/x/5cdf.txt', 'a/y/5cdf.txt', 'a/x/1cef.txt', 'a/y/1cef.txt', 'a/x/2cef.txt', 'a/y/2cef.txt', 'a/x/3cef.txt', 'a/y/3cef.txt', 'a/x/4cef.txt', 'a/y/4cef.txt', 'a/x/5cef.txt', 'a/y/5cef.txt' ]);
+    });
+    it('should expand a combination of nested sets and ranges:', function () {
       expand('a/{x,{1..5},y}/c{d}e').should.eql(['a/x/cde', 'a/1/cde', 'a/y/cde', 'a/2/cde', 'a/3/cde', 'a/4/cde', 'a/5/cde']);
     });
   });
