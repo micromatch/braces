144d2fae9d7bc213ca8532104e138f99bc81c889
jonschlinkert <dev@sellside.com>
2014-11-25 15:02:05 -0500

    don't run mm comparison tests by default

--- test/bash.js
+++ test/bash.js
@@ -8,13 +8,13 @@ var expand = require('..');
 var failing = require('./fixtures/failing');
 var spec = require('./fixtures/spec');
 
-// describe('braces: bash 4.3 support (passing)', function () {
-//   spec.forEach(function(test, i) {
-//     it('test: ' + i, function () {
-//       expand(test[0]).should.eql(test[1]);
-//     });
-//   });
-// });
+describe('braces: bash 4.3 support (passing)', function () {
+  spec.forEach(function(test, i) {
+    it('test: ' + i, function () {
+      expand(test[0]).should.eql(test[1]);
+    });
+  });
+});
 
 // describe('braces: bash 4.3 support (failing)', function () {
 //   failing.forEach(function(test, i) {
@@ -24,18 +24,18 @@ var spec = require('./fixtures/spec');
 //   });
 // });
 
-describe('minimatch: bash 4.3 support (passing)', function () {
-  spec.forEach(function(test, i) {
-    it('test: ' + i, function () {
-      mm(test[0]).should.eql(test[1]);
-    });
-  });
-});
+// describe('minimatch: bash 4.3 support (passing)', function () {
+//   spec.forEach(function(test, i) {
+//     it('test: ' + i, function () {
+//       mm(test[0]).should.eql(test[1]);
+//     });
+//   });
+// });
 
-describe('minimatch: bash 4.3 support (failing)', function () {
-  failing.forEach(function(test, i) {
-    it('test: ' + i, function () {
-      mm(test[0]).should.eql(test[1]);
-    });
-  });
-});
+// describe('minimatch: bash 4.3 support (failing)', function () {
+//   failing.forEach(function(test, i) {
+//     it('test: ' + i, function () {
+//       mm(test[0]).should.eql(test[1]);
+//     });
+//   });
+// });
