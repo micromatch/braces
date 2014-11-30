1bd6d9da567949d8e365dd39a7b786e4f7aba5c6
jonschlinkert <dev@sellside.com>
2014-11-30 16:12:29 -0500

    update fixtures/tests

--- examples.js
+++ examples.js
@@ -14,28 +14,65 @@ var braces = require('./');
  * @return {Array}
  */
 
-function expand1(arr) {
-  arr = Array.isArray(arr) ? arr : [arr];
+// function expand1(arr) {
+//   arr = Array.isArray(arr) ? arr : [arr];
 
-  return arr.reduce(function (acc, str) {
-    return acc.concat(braces(str));
-  }, [])
-}
+//   return arr.reduce(function (acc, str) {
+//     return acc.concat(braces(str));
+//   }, [])
+// }
 
-console.log(expand1(['{foo,bar}', '{baz,quux}', '{a,{a-{b,c}}}']));
+// console.log(expand1(['{foo,bar}', '{baz,quux}', '{a,{a-{b,c}}}']));
 
 
-// faster but less readable
-function expand2(val) {
-  val = Array.isArray(val) ? val : [val];
-  var len = val.length;
-  var arr = [];
-  var i = 0;
+// // faster but less readable
+// function expand2(val) {
+//   val = Array.isArray(val) ? val : [val];
+//   var len = val.length;
+//   var arr = [];
+//   var i = 0;
 
-  while (i < len) {
-    arr = arr.concat(braces(val[i++]));
-  }
-  return arr;
-}
+//   while (i < len) {
+//     arr = arr.concat(braces(val[i++]));
+//   }
+//   return arr;
+// }
 
-console.log(expand2(['{foo,bar}', '{baz,quux}', '{a,{a-{b,c}}}']));
+// console.log(expand2(['{foo,bar}', '{baz,quux}', '{a,{a-{b,c}}}']));
+
+
+console.log(braces("0{1..9} {10..20}"))
+
+// module.exports = [
+//   // minimatch passes this one
+//   ["{0..10,braces}", ['0..10', 'braces']],
+//   ["0{1..9} {10..20}", ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']],
+
+//   // minimatch passes this one
+//   // braces => ['a-bd-c', 'a-be-c']
+//   ["a-{b{d,e}}-c", ['a-{bd}-c', 'a-{be}-c']],
+
+//   // I have no idea why this is considered a good, expected result in bash
+//   ["a-{bdef-{g,i}-c", ['a-{bdef-g-c', 'a-{bdef-i-c']],
+//   ['{"klklkl"}{1,2,3}', ['{klklkl}1', '{klklkl}2', '{klklkl}3']],
+//   ['{"x,x"}', ['{x,x}']],
+
+//   // braces => [ 'x', 'y', 'abc', 'trie' ]
+//   ["{x,y,{abc},trie}", ['x,y', '{abc}', 'trie']],
+
+//   ["{a,b}{{a,b},a,b}", ['aa ab aa ab ba bb ba bb']],
+
+//   // braces => ['']
+//   ["{ }", ['{', '}']],
+
+//   // braces => ['foo 1 bar', 'foo 2 bar']
+//   ["foo {1,2} bar", ['foo', '1', '2', 'bar']],
+
+//   // minimatch passes
+//   ["{0..10,braces}", ['0..10', 'braces']],
+
+//   // minimatch passes
+//   // braces (only the sort order is different) => ['0', 'braces', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+//   ["{{0..10},braces}", ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'braces']],
+//   ["x{{0..10},braces}y", ['x0y', 'x1y', 'x2y', 'x3y', 'x4y', 'x5y', 'x6y', 'x7y', 'x8y', 'x9y', 'x10y', 'xbracesy']],
+// ];
--- test/fixtures/failing.js
+++ test/fixtures/failing.js
@@ -1,7 +1,6 @@
 module.exports = [
   // minimatch passes this one
   ["{0..10,braces}", ['0..10', 'braces']],
-  ["0{1..9} {10..20}", ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']],
 
   // minimatch passes this one
   // braces => ['a-bd-c', 'a-be-c']
@@ -9,23 +8,15 @@ module.exports = [
 
   // I have no idea why this is considered a good, expected result in bash
   ["a-{bdef-{g,i}-c", ['a-{bdef-g-c', 'a-{bdef-i-c']],
+  ["{0..10,braces}", ['0..10', 'braces']],
+
   ['{"klklkl"}{1,2,3}', ['{klklkl}1', '{klklkl}2', '{klklkl}3']],
   ['{"x,x"}', ['{x,x}']],
 
   // braces => [ 'x', 'y', 'abc', 'trie' ]
   ["{x,y,{abc},trie}", ['x,y', '{abc}', 'trie']],
 
-  // braces => ['']
-  ["{ }", ['{', '}']],
-
-  // braces => ['foo 1 bar', 'foo 2 bar']
-  ["foo {1,2} bar", ['foo', '1', '2', 'bar']],
+  ["{a,b}{{a,b},a,b}", ['aa ab aa ab ba bb ba bb']],
 
   // minimatch passes
-  ["{0..10,braces}", ['0..10', 'braces']],
-
-  // minimatch passes
-  // braces (only the sort order is different) => ['0', 'braces', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-  ["{{0..10},braces}", ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'braces']],
-  ["x{{0..10},braces}y", ['x0y', 'x1y', 'x2y', 'x3y', 'x4y', 'x5y', 'x6y', 'x7y', 'x8y', 'x9y', 'x10y', 'xbracesy']],
 ];
