a92889050e7a7b080749b1d5e6c1dee0e6556ef6
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-08 19:23:50 -0500

    add `mm-adjusted` tests, with tweaks to make the minimatch tests more appropriate for node.js (versus Bash 4.3 command line focused)

--- test/bash.js
+++ test/bash.js
@@ -3,13 +3,13 @@
 var path = require('path');
 var should = require('should');
 var argv = require('minimist')(process.argv.slice(2));
-var fixtures = require('./fixtures/bash-node');
+var fixtures = require('./fixtures/bash-mm-adjusted');
 var mm;
 
 if ('minimatch' in argv) {
   mm = require('minimatch').braceExpand;
 } else {
-  mm = require('..');
+  mm = require('../temp');
 }
 
 if (argv.t) {
@@ -23,8 +23,9 @@ function expand(pattern) {
 describe('bash', function () {
   describe('bash 4.3 support', function () {
     fixtures.forEach(function(test, i) {
-      it('test: ' + i, function () {
-        mm(test[0]).sort().should.eql(test[1].sort());
+      // adjust i for line numbers
+      it('test: ' + (i + 3), function () {
+        mm(test[0], {clean: true}).sort().should.eql(test[1].sort());
       });
     });
   });
