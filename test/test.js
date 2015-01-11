551e52a9bbb468879a3c95a9066d0409c373264e
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-11 10:05:42 -0500

    lint

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 /*!
  * braces <https://github.com/jonschlinkert/braces>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015, Jon Schlinkert.
  * Licensed under the MIT License
  */
 
