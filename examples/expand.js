012e4b54e72d82bda889650320002094b390bdf1
jonschlinkert <jon.schlinkert@sellside.com>
2017-04-11 21:10:40 -0400

    clear queue at the end
    move some logic to utils so it's testable

--- examples/expand.js
+++ examples/expand.js
@@ -5,3 +5,7 @@ var braces = require('..');
 
 console.log(braces.expand('a/{b,c}/d'));
 //=> [ 'a/b/d', 'a/c/d' ]
+
+var ast = braces.parse('a${b{a,b}}c', {sourcemap: true});
+var res = braces.compile(ast);
+console.log(res.ast)
